df6170635f4a7407ea841711d98557c43138c4ec
Mohitjangra001 <mohitjangra883@gmail.com>
2024-11-09 01:32:05 +0530

    done

--- src/component/Question.js
+++ src/component/Question.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import "./Question.css"
+import React, { useState } from 'react';
+import "./Question.css";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+
 
 function Question({ question, answer, isLast }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAnswer = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="question-container">
-      <h2 className="question-title">{question}</h2>
-      <p className="question-answer">{answer}</p>
+      <h2 className="question-title" onClick={toggleAnswer}>
+        {question}
+        <span className="accordion-icon">{isOpen ? <MdOutlineKeyboardArrowDown />
+          : <MdKeyboardArrowLeft />
+        }</span>
+      </h2>
+      {isOpen && <p className="question-answer">{answer}</p>}
       {!isLast && <div className="question-divider"></div>}
     </div>
   );
